ef135f88f8fdf7cbb35725fd76d1cc15b429b348
La-Pastelitos <lapastelititos@gmail.com>
2025-11-15 01:24:14 -0400

    resolução de erros

--- cardapio-main.js
+++ cardapio-main.js
@@ -1,3 +1,16 @@
+function mostrarCategoria(id) {
+  document.querySelectorAll(".categoria").forEach((sec) => {
+    sec.classList.remove("ativa");
+  });
+  document.getElementById(id).classList.add("ativa");
+}
+const botoes = document.querySelectorAll(".nav-btn");
+botoes.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    botoes.forEach((b) => b.classList.remove("btn-nav-ativo"));
+    btn.classList.add("btn-nav-ativo");
+  });
+});
 // Selecionar modal e overlay
 const modal = document.getElementById("modal-produto");
 const overlay = document.getElementById("overlay");
